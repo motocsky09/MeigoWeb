00f0fb68585d30cc88724754a25b4d815be49fa1
motocsky09 <motoc.andreialexandru@gmail.com>
2024-08-08 23:42:43 +0300

    Profile implements fix bug & design modify

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd6fb1cb4954bb043efce821bf77dda4db757056")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+908dc33413a256e95029a192ce6403e936a1a406")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
