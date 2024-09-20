ea469e352613e7765bb2d9494ed3d223f4be1c58
motocsky09 <emailul.tau@example.com>
2024-09-20 21:21:28 +0300

    Change logo & color design

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+561593d6b02f216b0c5c6f97a125a28a3f6939e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8af02814ffe5bb6b82e25b1a5002143fefad3b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
