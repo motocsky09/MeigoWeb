24ec417a08e438c9f9de7f2f4d9b41c0f6766586
motocsky09 <motoc.andreialexandru@gmail.com>
2024-12-29 14:38:40 +0200

    Mobile shopping cart design

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7256d957a8cac6a7708d38731ebfe7bdc1dc5a2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df9da04846e79f2b8c8a11b6a29b93977b80e1ce")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
