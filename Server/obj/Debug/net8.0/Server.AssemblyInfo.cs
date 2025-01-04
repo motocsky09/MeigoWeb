bcf8e7e5257a91a097a735e16f46a647a435c9dc
motocsky09 <motoc.andreialexandru@gmail.com>
2025-01-05 01:17:00 +0200

    Product page layout change

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21aca610382f9c00ec7257f71cd25515aed8545f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ace9a830e0ded08b052d61dab89a1e1504521bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
