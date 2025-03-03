6e43d0b6278b6e1b0aff979f361e70ecb5da9060
motocsky09 <motoc.andreialexandru@gmail.com>
2025-03-03 19:55:08 +0200

    Screen update

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2dbddffe3b10dff750091eb536ed8eea023df8b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7907394d4856408cff7db31d2752ef24e4d07b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
