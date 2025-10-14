ed8ea5170c1bf9bb4daf8363883574167edcebe7
motocsky09 <motoc.andreialexandru@gmail.com>
2025-10-15 02:07:37 +0300

    fix filter color

--- Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net8.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3d116e6d43efbfcccde76d517fbcc661e251341")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+314e7d879e880398b791f817586e4f3fb33143e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
